a9fd51ace07c3d704729bba38ecff941aaf848a3
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-06 16:01:41 -0400

    preloader active.

--- js/script.js
+++ js/script.js
@@ -6,24 +6,12 @@
 // Make function available after document is ready.
 // executes before window on load
 $(document).ready(() => {
-// OR $(() => { })
-    $('h1').hide(2000); 
-
-    $('h1').show(2000);
-
-    //$('h1').slideup(1000).slidedown(1000);
 
     // Select ID
     $('#main').css({
         color: 'red',
         fontSize: 25
     });
-
-    // Select by class
-    /* $('.content').css({
-        color: 'blue',
-        fontSize: 50
-    });*/
 
     // On click event handler
     $('h1').click(() => {

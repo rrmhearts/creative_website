3e63463292dfc9313ab516f8046a5d212831ae6a
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-06 16:13:18 -0400

    preloader fades out

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
-/* How to use JQuery */
-// $(selector).action();
-
-
-
-// Make function available after document is ready.
-// executes before window on load
-$(document).ready(() => {
-
-    // Select ID
-    $('#main').css({
-        color: 'red',
-        fontSize: 25
-    });
-
-    // On click event handler
-    $('h1').click(() => {
-        $('.content').css({
-            color: 'blue',
-            fontSize: 50
-        });
-    });
-});
-
-// Everything fully loaded including images / content
+/********************************
+Preloader
+*********************************/
 $(window).on("load", () => {
-// including graphics
-    //alert("Window is loaded!");
+    $('#status').delay(350).fadeOut();
+    $('#preloader').delay(350).fadeOut();
 });

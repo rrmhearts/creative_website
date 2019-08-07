37b984e575b18641a42bb58d303f7549db23dbe7
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-07 14:32:09 -0400

    basic carousel in place w/ teams

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ $(window).on("load", () => {
     $('#status').delay(350).fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+//$(document).ready(function(){
+$( () => {
+    $('#team-members').owlCarousel({
+        loop:true,
+        autoplay: true,
+        smartSpeed: 600,
+        margin:0,
+        autoplayHoverPause: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:2,
+                nav:true,
+                dots: false,
+                navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            990:{
+                items:2,
+                nav:false,
+            }
+        }
+    });
+  });

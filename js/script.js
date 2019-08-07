35337887d6fc1fa09a24bb703a9edf8ef0808397
Ryan <rrmhearts@gmail.com>
2019-08-07 01:47:57 +0000

    finished home section.

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ Preloader
 *********************************/
 $(window).on("load", () => {
     $('#status').delay(350).fadeOut();
-    $('#preloader').delay(350).fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
 });

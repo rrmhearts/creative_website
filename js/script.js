62cabcfc413a1e4ed01fdae61069e8b321f5357c
Ryan McCoppin <ryan.mccoppin@averydennison.com>
2019-08-08 16:35:18 -0400

    finished skills. Automate sliders.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 /********************************
-Preloader
+* Preloader
 *********************************/
 $(window).on("load", () => {
     $('#status').delay(350).fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
 
+/********************************
+ * Owl Carousel
+ *******************************/
 //$(document).ready(function(){
 $( () => {
     $('#team-members').owlCarousel({
@@ -29,7 +32,30 @@ $( () => {
             990:{
                 items:2,
                 nav:false,
+            },
+            1600:{
+                items:3,
+                nav:false,
             }
         }
+    });
+  });
+
+  /******************************************
+   * Progress Bars
+   *****************************************/
+  $( () => {
+
+    $('#progress-elements').waypoint(() => {
+        $('.progress-bar').each( (i, obj) => {
+            let newWidth = ($(obj).attr('aria-valuenow') + '%');
+            $(obj).animate({
+                'width': newWidth
+            }, 1500);
+        });
+        this.destroy();
+    },
+    {
+        offset: 'bottom-in-view'
     });
   });

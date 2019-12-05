ba691870ed65326b020cbc6633f6cacbf992fb54
Ryan <rrmhearts@gmail.com>
2019-12-05 12:34:02 -0500

    Finished contact, map and footer of website.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@
 $(window).on("load", () => {
     $('#status').delay(350).fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
+
+
+    // Initialize and add the map
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+
+    var infowindow = new google.maps.InfoWindow({
+        content: "Uluru Place"
+    });
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    })
 });
 
 // Document load
